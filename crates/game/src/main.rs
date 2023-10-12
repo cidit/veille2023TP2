a4b585fd4464fa4b83c6969c2a9b0e4e356669a8
Philippe Vallerand <pvallerand1@gmail.com>
2023-10-12 18:00:08 -0400

    add fn for flip perso on cursor pos + cam pos ply

--- crates/game/src/main.rs
+++ crates/game/src/main.rs
@@ -1,5 +1,6 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
 use bevy_rapier2d::prelude::*;
+
 
 fn main() {
     App::new()
@@ -15,12 +16,18 @@ fn main() {
 #[derive(Component)]
 struct Player;
 
+#[derive(Component)]
+struct MainCamera;
+
+
 pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(FixedTime::new_from_secs(1.0 / 60.0)) // 1 60th of a second, for 60FPS
             .add_systems(Startup, setup)
             .add_systems(FixedUpdate, move_player)
+            .add_systems(Update, rotate_player_according_to_mouse)
+            .add_systems(Update, camera_follow_player)
             .add_systems(Update, bevy::window::close_on_esc);
     }
 }
@@ -29,7 +36,9 @@ fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
 ) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(
+     (Camera2dBundle::default(), MainCamera)
+    );
     commands.spawn((
         Player,
         RigidBody::Dynamic,
@@ -47,6 +56,12 @@ fn setup(
             //     custom_size: Some(Vec2::new(20.0, 20.0)),
             //     ..Default::default()
             // },
+            sprite: Sprite {
+                flip_x: true,
+                flip_y: false,
+                ..Default::default()
+            },
+
             transform: Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(3.0)),
             ..default()
         },
@@ -86,5 +101,38 @@ fn move_player(
     for mut impulse in impulses.iter_mut() {
         impulse.impulse = movement * PLAYER_SPEED * time_step.period.as_secs_f32();    
     }
+    
+}
+
+
+fn rotate_player_according_to_mouse(
+    player_transform: Query<&Transform, With<Player>>,
+    windows: Query<&Window, With<PrimaryWindow>>,
+    mut sprite: Query<&mut Sprite, With<Player>>,
+    q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+) {
+    let (camera, camera_transform) = q_camera.single();
+
+    if let Some(position) = windows.single().cursor_position()
+        .and_then(| cursor | camera.viewport_to_world(camera_transform, cursor))
+        .map(| ray | ray.origin.truncate())
+     {
+        if player_transform.single().translation.x < position.x {
+            sprite.single_mut().flip_x = false;
+        }
+        else {
+            sprite.single_mut().flip_x = true;
+        }
+    }
+}
+
+fn camera_follow_player(
+    player_transform: Query<&Transform, With<Player>>,
+    mut camera_transform: Query<&mut Transform, (With<MainCamera>, Without<Player>)>,
+) {
+    let player_transform = player_transform.single();
+    let mut camera_transform = camera_transform.single_mut();
+    camera_transform.translation.x = player_transform.translation.x;
+    camera_transform.translation.y = player_transform.translation.y;
     
 }
--- journal/baie-st-paul/init.md
+++ journal/baie-st-paul/init.md
@@ -6,3 +6,13 @@ conceptes de rust pour m'habituer au langage
 
 lien 1: https://www.youtube.com/watch?v=br3GIIQeefY&t=2s
 lien 2: https://doc.rust-lang.org/rust-by-example/
+
+
+# 12 Octobre 2023
+
+j'ai commencer a travailler sur le projet et j'ai commencer a faire le code pour le projet. j'ai ecrit une fonction qui fait changer la direction que le personnage regarde selon la position du curseur et j'ai fait une fonction qui fait que la camera suit la position du personnage
+
+lien 1: https://bevy-cheatbook.github.io/input/mouse.html
+lien 2: https://doc.rust-lang.org/std/option/enum.Option.html#variant.Some
+
+
